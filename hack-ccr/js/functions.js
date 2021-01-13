bf382a227eca2921a968d81f1138d8c737a79e6f
Evelyn <evelynhelena781@gmail.com>
2021-01-13 19:41:50 -0300

    Fazendo form de login

--- hack-ccr/js/functions.js
+++ hack-ccr/js/functions.js
@@ -7,11 +7,26 @@ $(function(){
 var functions = new Vue({
     el: '#app',
     data: {
+      aluno: true,
+      professor: false,
+      login: false,
+      cadastro: true,
+      forgot: false,
       school: [
         {id: 1, name: "Escolaridade"},
         {id: 2, name: "Ensino Fundamental"},
         {id: 3, name: "Ensino Médio"},
         {id: 4, name: "Ensino Superior"},
+      ],
+      materia: [
+        {id: 1, name: "Matéria"},
+        {id: 2, name: "Português"},
+        {id: 3, name: "Matemática"},
+        {id: 4, name: "História"},
+        {id: 5, name: "Geografia"},
+        {id: 6, name: "Física"},
+        {id: 7, name: "Química"},
+        {id: 8, name: "Inglês"},
       ],
       finalPreco: 0,
     },
